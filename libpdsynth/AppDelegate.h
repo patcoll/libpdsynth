466a378ddebe1c8fa6012bd75fc0ce5c80944b3e
Pat Collins <collins@maya.com>
2012-01-30 16:53:41 -0500

    Initial Commit

---
+++ libpdsynth/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  libpdsynth
+//
+//  Created by Patrick Collins on 1/30/12.
+//  Copyright (c) 2012 MAYA Design, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ libpdsynth/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  libpdsynth
+//
+//  Created by Patrick Collins on 1/30/12.
+//  Copyright (c) 2012 MAYA Design, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
